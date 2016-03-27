fa963cf5a692fbd283408123c123015dd616b951
Julian Whitman <julian.whitman@gmail.com>
2016-03-27 16:59:10 -0400

    quad_scratchings4 now works to make the actual robot move.

--- quadrupedGait/quad_scratchings4.m
+++ quadrupedGait/quad_scratchings4.m
@@ -2,7 +2,7 @@
 % With simulation and sendCommands option.
 
 simulation = 1; %0 to turn off simulation
-sendCommands = 0; % 1 to turn on commands to real robot
+sendCommands = 1; % 1 to turn on commands to real robot
 
 addpath(genpath('C:\Users\medgroup01\Documents\Julian\snakeMonster\KDC_Project'));
 % addpath(genpath('C:\Users\Julian\Box Sync\CMU sem 1+2\snakeMonster\KDC_project'));
@@ -14,89 +14,39 @@ inch2m = 0.0254;
 odd = [1,3,5]; even= [2 4 6];
 x =1; y =2; z = 3;
 
-% initialize hebi object
-chassis_bend= 1.5; % degrees
-% transformation from the origin to the leg frame
-baseTransform = zeros(4,4,6);
-baseTransform(:,:,1) = [R_y(deg2rad(90-chassis_bend))*R_z(deg2rad(90))  [params.W/2; params.L/2; 0]; [0 0 0 1]];
-baseTransform(:,:,2) = [R_y(deg2rad(-90+chassis_bend))*R_z(deg2rad(90)) [-params.W/2; params.L/2; 0]; [0 0 0 1]];
-baseTransform(:,:,3) = [R_y(deg2rad(90-chassis_bend))*R_z(deg2rad(90))  [params.W/2; 0; 0]; [0 0 0 1]];
-baseTransform(:,:,4) = [R_y(deg2rad(-90+chassis_bend))*R_z(deg2rad(90)) [-params.W/2; 0; 0]; [0 0 0 1]];
-baseTransform(:,:,5) = [R_y(deg2rad(90-chassis_bend))*R_z(deg2rad(90))  [params.W/2; -params.L/2; 0]; [0 0 0 1]];
-baseTransform(:,:,6) = [R_y(deg2rad(-90+chassis_bend))*R_z(deg2rad(90)) [-params.W/2; -params.L/2; 0]; [0 0 0 1]];
-baseXYZ = getXYZ(baseTransform);
+kin = SnakeMonsterKinematics; % does all the kinamatics
 
-legKin = cell(1,6);
-frames = cell(1,6);
-lastLinkTwist = [pi 0 pi 0 pi 0]-pi/2;
-legCoM = zeros(3,7); % last entry is the chassis
-masses = zeros(7,1); % last entry is the chassis
-masses(end) = params.robotMass;
-effectors= zeros(3,6); % xyz of effectors
-for i = 1:6
-legKin{i} = HebiKinematics();
-legKin{i}.setBaseFrame(baseTransform(:,:,i));
-legKin{i}.addBody('FieldableElbowJoint');
-legKin{i}.addBody('FieldableElbowJoint');
-legKin{i}.addBody('FieldableStraightLink', ...
-    'ext', params.staticLength1-.0122, 'twist', pi/2);
-legKin{i}.addBody('FieldableElbowJoint');
-legKin{i}.addBody('FieldableElbowLink', ...
-    'ext1', params.staticLength2 - 0.0360, 'twist1', lastLinkTwist(i), ...
-    'ext2', params.staticLength3 - 0.0336, 'twist2', 0);
-legKin{i}.addBody('FieldableStraightLink', ...
-    'ext', params.footLength-0.0122, 'twist', 0);
-
- frames{i} = legKin{i}.getForwardKinematics('output', th0(:,i));
-  CoMs = legKin{i}.getForwardKinematics('CoM', th0(:,i));
-  legCoM(:,i) = getXYZ(CoMs)*legKin{i}.getBodyMasses;
-  masses(i) = sum(legKin{i}.getBodyMasses);
-effectors(:,i) = frames{i}(1:3,4,end);
-end
-% effectors
-
-th_IK = zeros(3,6);
-xyz0 = effectors;
-xyz = effectors;
+xyz0 = kin.getLegPositions(th0);
+xyz = xyz0;
 
 % move all feet in towards chassis
 xyz(1,odd) = xyz(1,odd)-.1;
 xyz(1,even) = xyz(1,even)+.1;
 xyz(3,:) = xyz(3,:)+.05;
 
-
-if simulation
-    close all;
-plt = SnakeMonsterPlotter(); hold on; 
-scatterCoM= scatter3(legCoM(1,:), legCoM(2,:), legCoM(3,:), 'k');
-projectedCOM = scatter3(0,0,0,'k', 'filled');
-supportLines = plot3(0,0,0,'k');
-scatterCentroid = scatter3(0,0,0,'b');
-end
-
-if sendCommands
-    setupSnakeMonsterGroups;
-end
-% command structure for if sending commands
-      cmd = CommandStruct();
-        cmd.position = [];
-        cmd.velocity = [];
-        cmd.torque = [];
         
-        
+t = 0;
 a = params.L/3; % step length = 2*a
  b = .05; % step height = b
  z0 = ones(1,6)*-.15;
 
- y0 = zeros(1,6);
- y0(odd)= [0 2*a a];
- y0(even) = [a 0 -a];
+ y0 = [params.L/2 params.L/2 0 0 -params.L/2 -params.L/2];
+ y0(odd)=y0(odd)+ [0 2*a 0];
+ y0(even) =  y0(even) +[3*a 0 -3*a];
+% y0(odd) = [params.L/2 0 -params.L/2];
+% y0(even) = [params.L/2 0 -params.L/2];
  
    swingLegs = zeros(1,6); % 1 indicates leg is in the air 
  walkingLegs = [ 2 3 4 5 6]; % specify which legs will be used for walking
  nWalkingLegs = length(walkingLegs);
  fractionStep = 1/nWalkingLegs;
- stepOrderBase = [6 1 5 2 3 4];
+%  stepOrderBase = [1 2 3 4 5 6]; % in order
+%  stepOrderBase = [6 4 2 5 3 1]; % back to front
+%   stepOrderBase = [1 3 5 2 4 6]; % fornt to back
+%    stepOrderBase = [1 2 3 4 6 5]; % works ok
+%    stepOrderBase = [1 4 3 6 2 5]; % works better
+stepOrderBase = [1 4 2 3 6  5]; % 
+
  stepOrder = [];
  extraLegs = [];
  % remove the non-walking legs from the step order
@@ -117,28 +67,79 @@ a = params.L/3; % step length = 2*a
  end
 
   % get initial IK
-  for i = 1:6
-  th_IK(:,i) = legKin{i}.getInverseKinematics('xyz', xyz(:,i));
-  frames{i} = legKin{i}.getForwardKinematics('output', th_IK(:,i));
-  effectors(:,i) = frames{i}(1:3,4,end);
-  CoMs = legKin{i}.getForwardKinematics('CoM', th_IK(:,i));
-  legCoM(:,i) = getXYZ(CoMs)*legKin{i}.getBodyMasses/sum(legKin{i}.getBodyMasses);
-  end    
+  
+   t_leg = t+2*pi/nWalkingLegs*(nWalkingLegs:-1:1);
+    % find the point on the eliptical path
+    for i = 1:nWalkingLegs
+        leg = stepOrder(i);
+        [xyz(2,leg),xyz(3,leg)] = ellipticalGait(a,b,y0(leg),z0(leg), fractionStep, t_leg(i));
+        if (mod(t_leg(i), 2*pi)<2*pi*fractionStep)&&(mod(t_leg(i), 2*pi)>0)
+            swingLegs(leg) = 1;
+        else
+            swingLegs(leg) = 0;
+        end
+    end
+    
+%  for i = 1:6
+%   th(:,i) = legKin{i}.getInverseKinematics('xyz', xyz(:,i));
+%   frames{i} = legKin{i}.getForwardKinematics('output', th(:,i));
+%   effectors(:,i) = frames{i}(1:3,4,end);
+%   CoMs = legKin{i}.getForwardKinematics('CoM', th(:,i));
+%   legCoM(:,i) = getXYZ(CoMs)*legKin{i}.getBodyMasses/sum(legKin{i}.getBodyMasses);
+%   end    
 
-  nCycles = 2;
+th = kin.getIK(xyz);
+effectors = kin.getLegPositions(th);
+CoMs = kin.getCenterOfMasses(th);
+masses = kin.getLegMasses();
+legCoM = zeros(3,7);
+for i = 1:6
+  legCoM(:,i) = CoMs(:,:,i)*masses(:,i)/sum(masses(:,i));
+end
+ legCoM(:,end) = legCoM*[sum(masses) params.robotMass].' /...
+     sum([sum(masses) params.robotMass]); % full body COM
+
+if simulation
+    close all;
+plt = SnakeMonsterPlotter(); hold on; 
+set(gcf, 'position', [100 100 800 800]);
+scatterCoM= scatter3(legCoM(1,:), legCoM(2,:), legCoM(3,:), 'k');
+projectedCOM = scatter3(0,0,0,'k', 'filled');
+supportLines = plot3(0,0,0,'k');
+scatterCentroid = scatter3(0,0,0,'b');
+view([0 0 -1]);
+ plt.plot(reshape(th,[1,18]));
+end
+
+ if sendCommands
+         setupSnakeMonsterGroup; % makes the snakeMonster hebi group object
+         setGainsSM; % set the gains
+          cmd.position = reshape(th,[1,18]);
+      snakeMonster.set(cmd);
+disp('press any key to start')
+      pause;
+ end
+ % command structure for if sending commands
+      cmd = CommandStruct();
+        cmd.position = [];
+        cmd.velocity = [];
+        cmd.torque = [];
+  
+  nCycles = 4;
  
- for t = linspace(0,2*pi*nCycles,100*nCycles)
+ for t = linspace(0,2*pi*nCycles,25*nCycles)
 %  % find the jacobian
 %  legKin{6}.getJacobian('EndEffector', th(:,i))
 %  % find the gravity compensation torques
 %    legKin{6}.getGravCompTorques(th(:,i),gravity)
-    
+  
+
+   t_leg = t+2*pi/nWalkingLegs*(nWalkingLegs:-1:1);
     % find the point on the eliptical path
     for i = 1:nWalkingLegs
         leg = stepOrder(i);
-        t_leg = t+2*pi/nWalkingLegs*(i-1);
-        [xyz(2,leg),xyz(3,leg)] = ellipticalGait(a,b,y0(leg),z0(leg), fractionStep, t_leg);
-        if (mod(t_leg, 2*pi)<2*pi*fractionStep)&&(mod(t_leg, 2*pi)>0)
+        [xyz(2,leg),xyz(3,leg)] = ellipticalGait(a,b,y0(leg),z0(leg), fractionStep, t_leg(i));
+        if (mod(t_leg(i), 2*pi)<2*pi*fractionStep)&&(mod(t_leg(i), 2*pi)>0)
             swingLegs(leg) = 1;
         else
             swingLegs(leg) = 0;
@@ -146,18 +147,25 @@ a = params.L/3; % step length = 2*a
     end
     
  
- for i = 1:6
-  th_IK(:,i) = legKin{i}.getInverseKinematics('xyz', xyz(:,i));
-   frames{i} = legKin{i}.getForwardKinematics('output', th_IK(:,i));
-  effectors2(:,i) = frames{i}(1:3,4,end);
-  CoMs = legKin{i}.getForwardKinematics('CoM', th_IK(:,i));
-  legCoM(:,i) = getXYZ(CoMs)*legKin{i}.getBodyMasses/sum(legKin{i}.getBodyMasses);
- end
+%  for i = 1:6
+%   th(:,i) = legKin{i}.getInverseKinematics('xyz', xyz(:,i));
+%    frames{i} = legKin{i}.getForwardKinematics('output', th(:,i));
+%   effectors2(:,i) = frames{i}(1:3,4,end);
+%   CoMs = legKin{i}.getForwardKinematics('CoM', th(:,i));
+%   legCoM(:,i) = getXYZ(CoMs)*legKin{i}.getBodyMasses/sum(legKin{i}.getBodyMasses);
+%  end
 
- legCoM(:,end) = legCoM*masses/sum(masses); % full body COM
+th = kin.getIK(xyz);
+effectors = kin.getLegPositions(th);
+CoMs = kin.getCenterOfMasses(th);
+for i = 1:6
+  legCoM(:,i) = CoMs(:,:,i)*masses(:,i)/sum(masses(:,i));
+end
+ legCoM(:,end) = legCoM*[sum(masses) params.robotMass].' /...
+     sum([sum(masses) params.robotMass]); % full body COM
  
  % plot the support polygon
- xContact = xyz(:,~swingLegs);
+ xContact = effectors(:,~swingLegs);
  K = convhull(xContact(1:2,:).');
  xOrdered = xContact(1:2,K.');
  % check if COM in the support poluygon
@@ -166,8 +174,8 @@ a = params.L/3; % step length = 2*a
  xCentroid = mean(xOrdered(:,1:end-1),2);
  
   if simulation
- plt.plot(reshape(th_IK,[1,18]));
- scatter3(effectors2(1,:), effectors2(2,:), effectors2(3,:), 'r');
+ plt.plot(reshape(th,[1,18]));
+ scatter3(effectors(1,:), effectors(2,:), effectors(3,:), 'r');
  scatter3(xyz(1,:), xyz(2,:), xyz(3,:), [], swingLegs, 'filled');
  set(scatterCoM, 'xdata', legCoM(1,:), 'ydata',legCoM(2,:), 'zdata',legCoM(3,:));
  set(supportLines, 'xdata', xOrdered(1,:), 'ydata',xOrdered(2,:), ...
@@ -176,12 +184,27 @@ a = params.L/3; % step length = 2*a
  set(projectedCOM, 'xdata', legCoM(1,end), 'ydata',legCoM(2,end), 'zdata',mean(z0),...
      'markerFaceColor', [1 0 0]*~inSupport);
  set(scatterCentroid, 'xdata', xCentroid(1), 'ydata',xCentroid(2), 'zdata',mean(z0));
+% view([0 0 -1]);
   end
   
   if sendCommands
-      cmd.position = th_IK;
+      cmd.position = reshape(th,[1,18]);
       snakeMonster.set(cmd);
   end
  
-end
- 
+ end
+
+ if sendCommands
+     pause(3)
+ % go limp
+ cmd.position = nan(1,18);
+  cmd.velocity = nan(1,18);
+   cmd.torque = nan(1,18);
+   snakeMonster.set(cmd);
+ end
+   
+%    th = [0 0 0 0 0 0; ...
+%         0 0 0 0 0 0; ...
+%         0 0 0 0 0 0];
+    
+   
---
+++ quadrupedGait/setGainsSM.m
@@ -0,0 +1,68 @@
+%% Read and Set Gains
+% Iterate thru all the groups and set the gains to default
+      
+    gains = GainStruct(); 
+
+    numModules = snakeMonster.getNumModules();
+
+    % Maximum velocity of SEA joints is 33 rpm; we convert to rad/s here.
+    maxVelocityRadS = (33 * 2 * pi) / 60;
+    
+    maxTorque = 12; % N-m
+
+    gains.positionKp = ones(1, numModules) * 6.0;
+    gains.positionKi = ones(1, numModules) * 0;
+    gains.positionIClamp = ones(1, numModules) * 0;
+    gains.positionKd = ones(1, numModules) * 30;
+    gains.positionFF = ones(1, numModules) * 0;
+    gains.positionPunch = ones(1, numModules) * 0;
+    gains.positionDeadZone = ones(1, numModules) * 0.00;
+    gains.positionMinOutput = ones(1, numModules) * -maxTorque;
+    gains.positionMaxOutput = ones(1, numModules) * maxTorque;
+    gains.positionMinTarget = ones(1, numModules) * -(pi/2 + 0.2);
+    gains.positionMaxTarget = ones(1, numModules) * (pi/2 + 0.2);
+    gains.positionTargetLowpassGain = ones(1, numModules) * 1;
+    gains.positionOutputLowpassGain = ones(1, numModules) * 1;
+    gains.positionDOnError = ones(1, numModules) * 1;
+
+    gains.velocityKp = ones(1, numModules) * 0.2;
+    gains.velocityKi = ones(1, numModules) * .000;
+    gains.velocityIClamp = ones(1, numModules) * .0;
+    gains.velocityKd = ones(1, numModules) * 0;
+    gains.velocityFF = ones(1, numModules) * 1/maxVelocityRadS;
+    gains.velocityDeadZone = ones(1, numModules) * 0.05;
+    gains.velocityPunch = ones(1, numModules) * 0.00;
+    gains.velocityMinOutput = ones(1, numModules) * -1;
+    gains.velocityMaxOutput = ones(1, numModules) * 1;
+    gains.velocityMinTarget = ones(1, numModules) * -maxVelocityRadS;
+    gains.velocityMaxTarget = ones(1, numModules) * maxVelocityRadS;
+    gains.velocityTargetLowpassGain = ones(1, numModules) * 1;
+    gains.velocityOutputLowpassGain = ones(1, numModules) * 1;
+    gains.velocityDOnError = ones(1, numModules) * 1;
+
+    gains.torqueKp = ones(1, numModules) * 0.5;
+    gains.torqueKi = ones(1, numModules) * 0;
+    gains.torqueIClamp = ones(1, numModules) * 0;
+    gains.torqueKd = ones(1, numModules) * 25;
+    gains.torqueFF = ones(1, numModules) * 0.15;
+    gains.torqueDeadZone = ones(1, numModules) * 0.01;
+    gains.torquePunch = ones(1,numModules) * 0;
+    gains.torqueMinTarget = ones(1, numModules) * -maxTorque;
+    gains.torqueMaxTarget = ones(1, numModules) * maxTorque;
+    gains.torqueMinOutput = ones(1, numModules) * -1;
+    gains.torqueMaxOutput = ones(1, numModules) * 1;
+    gains.torqueTargetLowpassGain = ones(1, numModules) * 1;
+    gains.torqueOutputLowpassGain = ones(1, numModules) * .25;
+    gains.torqueDOnError = ones(1, numModules) * 0;
+   
+    % Set the Gains (NO PERSIST)
+    snakeMonster.set('gains', gains);
+    
+snakeMonster.setCommandLifetime(0);
+%     % Set the Gains (PERSIST)
+%     gainGroup.set('gains', gains, 'persist', true, 'led', 'y');
+
+% end
+
+pause(2.0);
+
